3be1bf39126a3b4ea579098f2da63297e04139b3
nichoth <nichoth@gmail.com>
2021-11-22 15:19:04 -0800

    test deploy

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello World");
+    return res.send("Hello Planet");
 });
 
 // listen to port 7000 by default
 app.listen(process.env.PORT || 7000, () => {
-  console.log("Server is running");
+    console.log("Server is running");
 });
 
 module.exports = app;
