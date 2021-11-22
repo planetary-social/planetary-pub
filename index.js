5431d434b69d3c3b76c59059a1500b644864e73e
nichoth <nichoth@gmail.com>
2021-11-22 11:35:27 -0800

    server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const http = require("http");
 
+const host = 'localhost';
+const port = 8000;
+
 const server = http.createServer(function (req, res) {
     res.writeHead(200);
     res.end("hello there");
