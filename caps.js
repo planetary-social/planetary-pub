8a38b92d56fb22f234ff30a6e55e9e3e1c9edcec
nichoth <nichoth@gmail.com>
2021-11-23 10:59:03 -0800

    add build script placeholder

--- caps.js
+++ caps.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // a random string
+  // crypto.randomBytes(32).toString('base64')
   "shs": "GI03TJXVGzJVxBe8RXXr4c2CAMcHzvpSxoi2xG9KxU0=",
   "sign": null,
   "invite": "HT0wIYuk3OWc2FtaCfHNnakV68jSGRrjRMP9Kos7IQc="
