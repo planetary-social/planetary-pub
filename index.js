4178ee85a63f03c7e8196aea7cbcbc687b4c9101
nichoth <nichoth@gmail.com>
2021-11-23 16:02:34 -0800

    better DB path

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const SecretStack = require('secret-stack')
 const caps = require('./caps')
 const { where, and, type, author, toCallback } = require('ssb-db2/operators')
 var http = require('http')
+const ssbKeys = require('ssb-keys')
+const path = require('path')
+const DB_PATH = process.env.DB_PATH || './db'
+
+//  '/var/db'
 
 const sbot = SecretStack({ caps })
     .use(require('ssb-db2'))
     // .use(require('ssb-db2/compat')) // include all compatibility plugins
-    .call(null, { path: '/var/db' })
+    .call(null, {
+        path: DB_PATH,
+
+        // the server has an identity
+        keys: ssbKeys.loadOrCreateSync(path.join(DB_PATH, 'secret'))
+    })
+
+// sbot.db.query(/*...*/)
 
 
 var server = http.createServer(function (req, res) {
     if (req.url === '/healthz') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         return res.end('Hello World!');
+    }
+
+    if (req.url === '/test') {
+        
     }
 
     res.writeHead(200, { 'Content-Type': 'text/plain' })
