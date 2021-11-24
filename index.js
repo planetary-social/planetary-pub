ed4f60210f5a2d4ef1fe27a70649fbdfaf81204a
nichoth <nichoth@gmail.com>
2021-11-23 16:07:48 -0800

    rm old express example code

--- index.js
+++ index.js
@@ -41,21 +41,3 @@ var server = http.createServer(function (req, res) {
 server.listen(8888);
 
 console.log('listening on 8888')
-
-
-// // importing express framework
-// const express = require("express");
-// const app = express();
-
-// // Respond with "hello world" for requests that hit our root "/"
-// app.get("/", function (req, res) {
-//     return res.send("Hello Planet earth");
-// });
-
-// // listen to port 7000 by default
-// app.listen(process.env.PORT || 7000, () => {
-//     console.log("Server is running");
-// });
-
-// module.exports = app;
-
