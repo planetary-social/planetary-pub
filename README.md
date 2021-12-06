806c7a711ee0cc30091fe1ef6fa5352cb689c9bd
nichoth <nichoth@gmail.com>
2021-12-06 12:40:02 -0800

    add test msgs

--- README.md
+++ README.md
@@ -2,12 +2,30 @@
 
 A pub
 
-See the [render.com deploy](https://dashboard.render.com/web/srv-c6elp2vh8vlcnlnvsm5g/settings)
+This creates an `sbot`, and imports and exposes the viewer app.
+
+See the [render.com dashboard](https://dashboard.render.com/web/srv-c6elp2vh8vlcnlnvsm5g/settings)
 
 https://pub2.onrender.com/
 
 See [how the caps value is generated](https://www.npmjs.com/package/ssb-caps#shs-secret-handshake-connection-key)
 
-## TODO
+-------------------------------------------------------
 
-* Write a viewer app
+example message
+
+```js
+ {
+  key: '%1HbhmsEc4OCiLD5o8raRl+x8QUO7Y6oZ3C57vwNM78c=.sha256',
+  value: {
+    previous: '%CCceib4KRShqtaN95hPGl0qORtefsTEb/qwbB0YZVyQ=.sha256',
+    sequence: 3,
+    author: '@lV5MISER9oGaZJ7OLhlsUNVWHu982USYgMEWfIs6le0=.ed25519',
+    timestamp: 1638820387617,
+    hash: 'sha256',
+    content: { type: 'post', text: 'three' },
+    signature: 'E2sC6mH9F+HhfIVl6MjobLdZX6RG3QRwFBiMoT5vb64L6XkS5TutPh2gYRRIqKZSzzW5ld0sLvvEc81pcrRtCQ==.sig.ed25519'
+  },
+  timestamp: 1638820387618
+}
+```
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const caps = require('./caps')
 const ssbKeys = require('ssb-keys')
 const path = require('path')
 var Viewer = require('@planetary-ssb/viewer')
+const {where, and, type, author, toCallback} = require('ssb-db2/operators')
+
+const user = require('./user.json')
+const userTwo = require('./user-two.json')
 
 const DB_PATH = process.env.DB_PATH || './db'
 const PORT = 8888
@@ -33,6 +37,46 @@ const sbot = SecretStack({ caps })
 
 console.log('sbot', sbot.config.keys.public)
 
+
+// can now add records to the DB
+sbot.db.query(
+    where(
+        and(
+            type('post'),
+            author(user.id)
+        )
+    ),
+    toCallback((err, msgs) => {
+        if (err) throw err
+        console.log('There are ' + msgs.length +
+            ' messages of type "post" from user')
+
+        // we already have test messages, no need to do anything
+        if (msgs.length) return
+
+        publishTestMsgs(user)
+    })
+)
+
+
+
+function publishTestMsgs (user) {
+    var testMsgs = [
+        { type: 'post', text: 'one' },
+        { type: 'post', text: 'two' },
+        { type: 'post', text: 'three' }
+    ]
+
+    testMsgs.forEach(msg => {
+        sbot.db.publishAs(user, msg, (err, res) => {
+            if (err) return console.log('errrrr', err)
+            console.log('published msg', res)
+        })
+    })
+}
+
+
+
 var viewer = Viewer(sbot)
 
 // add another route
@@ -40,6 +84,7 @@ viewer.get('/healthz', (_, res) => {
     res.send('ok')
 })
 
+// `fastify` API
 viewer.listen(PORT, '0.0.0.0', (err, address) => {
     if (err) throw err
     console.log(`Server is now listening on ${address}`)
