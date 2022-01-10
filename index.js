7587d19034f55a506687826045de63b922164b92
nichoth <nichoth@gmail.com>
2022-01-10 14:27:55 -0800

    fix server listening

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ var Pub = require('./pub')
 
 Pub(function (err, sbot) {
     if (err) throw err
-    Viewer(sbot)
+    var viewer = Viewer(sbot)
+
+    viewer.listen(8888, '0.0.0.0', (err, address) => {
+        next(err)
+        console.log(`Server is now listening on ${address}`)
+    })
 })
