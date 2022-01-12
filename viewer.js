99015b2325844ccde662cc8376dec5cded37c33c
nichoth <nichoth@gmail.com>
2022-01-12 14:18:11 -0800

    hide threads because they crash the server

--- viewer.js
+++ viewer.js
@@ -100,48 +100,49 @@ module.exports = function startServer (sbot) {
                 // TODO
                 // in here, get the blobs that are regerenced by messages
                 S.drain(msgs => {
-                    getThreads(msgs)
+                    // getThreads(msgs)
                     console.log('***got msgs***', msgs.length)
+                    res.send(msgs)
                     // now get the threads
                 })
             )
 
-            function getThreads (msgs) {
-                S(
-                    S.values(msgs),
+            // function getThreads (msgs) {
+            //     S(
+            //         S.values(msgs),
 
-                    S.map((msg) => {
-                        return sbot.threads.thread({
-                            root: msg.key,
-                            allowlist: ['post'],
-                            // threads sorted from most recent to
-                            // least recent
-                            reverse: true, 
-                            // at most 3 messages in each thread
-                            threadMaxSize: 3, 
-                        })
-                    }),
+            //         S.map((msg) => {
+            //             return sbot.threads.thread({
+            //                 root: msg.key,
+            //                 allowlist: ['post'],
+            //                 // threads sorted from most recent to
+            //                 // least recent
+            //                 reverse: true, 
+            //                 // at most 3 messages in each thread
+            //                 threadMaxSize: 3, 
+            //             })
+            //         }),
 
-                    S.flatten(),
+            //         S.flatten(),
 
-                    S.map(res => {
-                        // return either [post, post, ...]
-                        // or post (not in array)
-                        return res.messages.length > 1 ?
-                            res.messages :
-                            res.messages[0]
-                    }),
+            //         S.map(res => {
+            //             // return either [post, post, ...]
+            //             // or post (not in array)
+            //             return res.messages.length > 1 ?
+            //                 res.messages :
+            //                 res.messages[0]
+            //         }),
 
-                    S.collect((err, msgs) => {
-                        if (err) {
-                            return res.send(
-                                createError.InternalServerError(err))
-                        }
+            //         S.collect((err, msgs) => {
+            //             if (err) {
+            //                 return res.send(
+            //                     createError.InternalServerError(err))
+            //             }
 
-                        res.send(msgs)
-                    })
-                )
-            }
+            //             res.send(msgs)
+            //         })
+            //     )
+            // }
         })
     })
 
