44618076ca3709ad4ff34e2b2364ba38cdcd69a1
nichoth <nichoth@gmail.com>
2022-02-01 11:45:55 -0800

    stash pop

--- viewer/threads.js
+++ viewer/threads.js
@@ -1,12 +1,12 @@
 var S = require('pull-stream')
-const Ref = require('ssb-ref');
+const Ref = require('ssb-ref')
 const { where, type, descending,
     toPullStream, and, batch, hasRoot,
     isPrivate, isPublic } = require('ssb-db2/operators')
-const cat = require('pull-cat');
-const sort = require('ssb-sort');
+const cat = require('pull-cat')
+const sort = require('ssb-sort')
 
-const BATCH_SIZE = 75;
+const BATCH_SIZE = 75
 
 module.exports = publicSummary
 
@@ -31,16 +31,15 @@ function publicSummary ({ sbot }) {
                             where(
                                 and(
                                     hasRoot(root.key),
-                                    // filter,
-                                    (privately ? isPrivate() : isPublic()),
+                                    (privately ? isPrivate() : isPublic())
                                 )
                             ),
                             batch(BATCH_SIZE),
                             descending(),
-                            toPullStream(),
+                            toPullStream()
                         ),
                         removeMessagesFromBlocked,
-                        S.take(maxSize),
+                        S.take(maxSize)
                     )
                 ]),
                 S.take(maxSize + 1),
@@ -48,19 +47,21 @@ function publicSummary ({ sbot }) {
                     if (err2) return cb(err2)
                     const full = arr.length <= maxSize
                     sort(arr)
-                    if (arr.length > maxSize && arr.length >= 3) arr.splice(1, 1)
+                    if (arr.length > maxSize && arr.length >= 3) {
+                        arr.splice(1, 1)
+                    }
                     cb(null, { messages: arr, full })
                 })
-            );
-        };
-    };
+            )
+        }
+    }
 
     const IS_BLOCKING_NEVER = (_, cb) => {
-        cb(null, false);
-    };
+        cb(null, false)
+    }
 
     function removeMessagesFromBlocked (source) {
-        const isBlocking = sbot.friends?.isBlocking ?
+        const isBlocking = (sbot.friends)?.isBlocking ?
             sbot.friends.isBlocking :
             IS_BLOCKING_NEVER
 
@@ -72,9 +73,9 @@ function publicSummary ({ sbot }) {
                     dest: msg.value.author
                 }, (err, blocking) => {
                     if (err) cb(err)
-                    else if (blocking) cb(null, null);
+                    else if (blocking) cb(null, null)
                     else cb(null, msg)
-                });
+                })
             }),
             S.filter()
         )
@@ -90,13 +91,13 @@ function publicSummary ({ sbot }) {
 
     function getRootMsgId (msg) {
         if (msg?.value?.content) {
-            const fork = msg.value.content.fork;
-            const root = msg.value.content.root;
-            if (fork && Ref.isMsgId(fork)) return fork;
-            if (root && Ref.isMsgId(root)) return root;
+            const fork = msg.value.content.fork
+            const root = msg.value.content.root
+            if (fork && Ref.isMsgId(fork)) return fork
+            if (root && Ref.isMsgId(root)) return root
         }
         // this msg has no root so we assume this is a root
-        return msg.key;
+        return msg.key
     }
 
     function isUniqueMsgId (uniqueRoots) {
@@ -107,7 +108,7 @@ function publicSummary ({ sbot }) {
                 uniqueRoots.add(id)
                 return true
             }
-        };
+        }
     }
 
     /**
@@ -120,12 +121,12 @@ function publicSummary ({ sbot }) {
             source,
             S.asyncMap((id, cb) => {
                 sbot.db.getMsg(id, (err, msg) => {
-                    if (err) cb(null /* missing msg */);
-                    else cb(err, msg);
-                });
+                    if (err) cb(null /* missing msg */)
+                    else cb(err, msg)
+                })
             }),
             S.filter() // remove missing msg
-        );
+        )
     }
 
     return S(
@@ -148,14 +149,14 @@ function publicSummary ({ sbot }) {
         removeMessagesFromBlocked,
         S.asyncMap(nonBlockedRootToThread(3)),
         // S.asyncMap(this.nonBlockedRootToSummary(filterOperator, timestamps)),
-    );
-};
+    )
+}
 
 function isPublicType (msg) {
-    if (msg.meta?.private) return false;
-    if (msg.value.private) return false;
-    if (Array.isArray(msg.value.content?.recps)) return false;
-    return typeof msg?.value?.content !== 'string';
+    if (msg.meta?.private) return false
+    if (msg.value.private) return false
+    if (Array.isArray(msg.value.content?.recps)) return false
+    return typeof msg?.value?.content !== 'string'
 }
 
 // { allowList, blockList }
@@ -173,11 +174,11 @@ function makePassesFilter (opts) {
     if (opts.blocklist) {
         return function (msg) {
             opts.blocklist.every((type) => {
-                return (msg?.value?.content?.type !== type);
+                return (msg?.value?.content?.type !== type)
             })
         }
     }
 
-    return () => true;
+    return () => true
 }
 
